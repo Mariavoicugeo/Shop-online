f9863f6d9a8087329391a875416ac51a4bd868c5
maria <164060667+Mariavoicugeo@users.noreply.github.com>
2024-09-23 20:29:56 +0300

    update-3

--- index.js
+++ index.js
@@ -1,4 +1,99 @@
+const home=document.querySelector(".home");
+const shop=document.querySelector('.shop');
+const homePage=document.querySelector("#home-page");
+const shopPage=document.querySelector("#shop-page");
+const btnSizeL=document.querySelector(".btn-size-L");
+const btnSizeM=document.querySelector(".btn-size-M");
+const btnSizeXs=document.querySelector(".btn-size-Xs");
+const silver=document.querySelector(".silver");
+const rose=document.querySelector(".rose");
+const gold= document.querySelector(".gold");
+const singleProductPage=document.querySelector("#single-product-page");
+const backHome=document.querySelector('.back-home');
+const backShop=document.querySelector('.back-shop');
+const showMore=document.querySelector('.show-more');
 
+showMore.addEventListener("click", () => {
+   
+    shopPage.style.display = "flex";
+    homePage.style.display = "none";
+    singleProductPage.style.display = "none";
+
+   
+    shopPage.scrollIntoView({
+        behavior: "smooth", 
+        block: "start"      
+    });
+});
+backHome.addEventListener("click",()=>{
+    shopPage.style.display="none"
+    homePage.style.display="flex"
+    singleProductPage.style.display="none";
+    
+    homePage.scrollIntoView({
+        behavior: "smooth", 
+        block: "start"      
+    });
+})
+
+backShop.addEventListener("click",()=>{
+    shopPage.style.display="flex"
+    homePage.style.display="none"
+    singleProductPage.style.display="none";
+    
+    shopPage.scrollIntoView({
+        behavior: "smooth", 
+        block: "start"      
+    });
+})
+
+silver.addEventListener("click",()=>{
+    silver.classList.add("border");
+    rose.classList.remove("border");
+    gold.classList.remove("border");
+})
+
+rose.addEventListener("click",()=>{
+    silver.classList.remove("border");
+    rose.classList.add("border");
+    gold.classList.remove("border");
+})
+
+gold.addEventListener("click",()=>{
+    silver.classList.remove("border");
+    rose.classList.remove("border");
+    gold.classList.add("border");
+})
+
+btnSizeL.addEventListener("click",()=>{
+    btnSizeL.classList.add("active");
+    btnSizeM.classList.remove("active");
+    btnSizeXs.classList.remove("active");
+})
+
+btnSizeM.addEventListener("click",()=>{
+    btnSizeM.classList.add("active");
+    btnSizeL.classList.remove("active");
+    btnSizeXs.classList.remove("active");
+})
+
+btnSizeXs.addEventListener("click",()=>{
+    btnSizeXs.classList.add("active");
+    btnSizeM.classList.remove("active");
+    btnSizeL.classList.remove("active");
+})
+
+
+home.addEventListener("click",()=>{
+    shopPage.style.display="none"
+    homePage.style.display="flex"
+    singleProductPage.style.display='none';
+})
+shop.addEventListener("click",()=>{
+    shopPage.style.display="flex"
+    homePage.style.display="none"
+    singleProductPage.style.display='none';
+})
 
 const printeazaProdus=(product, productBox)=>{
     
@@ -27,27 +122,79 @@ const printeazaProdus=(product, productBox)=>{
        
        
         const hover=item.querySelector(".hover");
-        const card=item.querySelector(".card");
+        const itemContent = item.querySelector(".item");
 
 
-    item.addEventListener("mouseenter",()=>{
-    hover.classList.add("hoverd");
-})
-       
+        item.addEventListener("mouseenter", () => {
+            timer = setTimeout(() => {
+                hover.classList.add("hoverd");
+            }, 900); 
+        });
+    
+        item.addEventListener("mouseleave", () => {
+            clearTimeout(timer); 
+            hover.classList.remove("hoverd"); 
+        });
+
+      itemContent.addEventListener("click", () => {
+        showProductDetails(product);
+        singleProductPage.scrollIntoView({
+            behavior: "smooth", 
+            block: "start"      
+        });
+        productsDetalii(product);
+      });  
 
 }
 
 const printeazaProduse=(arrayProduse)=>{
     const primaCutie=document.querySelector('#first-box');
     const aDouaCutie=document.querySelector('#second-box');
+    const atreiaCutie=document.querySelector("#third-box");
+    const apatraCutie=document.querySelector("#forth-box");
+    const acinceaCutie=document.querySelector("#fifth-box");
+    const asaseaCutie=document.querySelector("#sixth-box");
     primaCutie.innerHTML='';
     aDouaCutie.innerHTML='';
+    atreiaCutie.innerHTML='';
+    apatraCutie.innerHTML='';
+    acinceaCutie.innerHTML='';
+    asaseaCutie.innerHTML='';
         arrayProduse.forEach((produs) => {
             printeazaProdus(produs , primaCutie);
             printeazaProdus(produs , aDouaCutie);
+            printeazaProdus(produs , atreiaCutie);
+            printeazaProdus(produs , apatraCutie);
+            printeazaProdus(produs , acinceaCutie);
+            printeazaProdus(produs , asaseaCutie);
         });
 }
 
+
+const showProductDetails = (product) => {
+  
+    homePage.style.display = "none";
+    shopPage.style.display = "none";
+    singleProductPage.style.display = "flex";
+    document.querySelector(".number-reviews").textContent=product.rating.count;
+    document.querySelector(".product-img").src= product.image;
+    document.querySelector("#title-product").textContent = product.title;
+    document.querySelector(".description").textContent = product.description;
+    document.querySelector("#price-product").textContent = `${product.price}$`;
+  };
+  const productsDetalii = (product) => {
+  
+   
+    
+    const images = document.querySelectorAll(".img-side");
+    images.forEach(img=>{
+        img.src=product.image;
+    })
+    document.querySelector("#bread-crum").textContent = product.title;
+   
+    
+  };
+  
 
 const fetchJewelery=async function (){
     try{
